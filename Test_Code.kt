533720da622130aa33510aac194959f82957a2fa
Nathan Tromp <103584549+NathanCabeiDiWebo@users.noreply.github.com>
2023-01-10 16:25:58 +0100

    patin

--- Test_Code.kt
+++ Test_Code.kt
@@ -1,3 +1,25 @@
+fun dolo(word: String){
+
+  var i = 0 
+  
+  
+  do {
+  println(word)
+  i++  
+  }
+  
+  while (i < 5)
+  
+  if (word == "cul"){
+    println("cul = ass")
+    return
+  
+  println("you are very retarded go back to school nigga")
+}
+}
+
 fun main() {
   println("Webo")
+  dolo("cul")
+  dolo("tonto")
 }
